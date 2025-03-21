e97b3005ac5e66bb881a43c201c668b8d849fb2e
Atmajjanardanan <aatmaj.janardanan@radiantgraph.com>
2025-03-21 14:38:05 -0700

    test AMD

--- vocode/streaming/telephony/client/twilio_client.py
+++ vocode/streaming/telephony/client/twilio_client.py
@@ -62,12 +62,6 @@ class TwilioClient(AbstractTelephonyClient):
             data["Record"] = "true"  # Enables recording
             data["RecordingChannels"] = "dual"  # Ensures separate caller & receiver channels
 
-        # data["MachineDetection"] = (
-        #     "Enable"  # Initiates AMD to identify if a human, machine, or fax answers
-        # )
-        # data["AsyncAmd"] = "true"  # Performs AMD asynchronously to minimize call delays
-        # data["AsyncAmdStatusCallback"]= 'https://voice-dev2.dev.radiantgraph.io/amd_callback'
-
         async with AsyncRequestor().get_session().post(
             f"https://api.twilio.com/2010-04-01/Accounts/{self.twilio_config.account_sid}/Calls.json",
             auth=self.auth,
